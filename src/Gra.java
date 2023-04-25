bfbf061c8a9ea9e1d7c003d7adacee95e2935d59
Michał Pilecki <michal190713@gmail.com>
2023-04-25 21:13:06 +0200

    poprawiono poruszanie

--- src/Gra.java
+++ src/Gra.java
@@ -4,27 +4,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-public class Gra extends JPanel implements KeyListener, ActionListener{
-    //stała pozycji początkowej
-    private int x = 300,y =470, poruszanie = 20;
+
+public class Gra extends JPanel implements KeyListener, ActionListener {
+    private int x = 300, y = 470, poruszanie = 20;
     private Timer time;
 
-    public Gra(){
-        //ustawienie czasu odświeżania gry
-        time = new Timer(10,this);
+    public Gra() {
+        time = new Timer(10, this);
         time.start();
 
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
-        //śledzenie jaki klawisz nacisnął użytkonik
         addKeyListener(this);
     }
-    //rysowanie sceny
-    public void paint(Graphics g){
+
+    public void paint(Graphics g) {
         g.setColor(Color.BLACK);
-        g.fillRect(0,0,700,600);
+        g.fillRect(0, 0, 700, 600);
         g.setColor(Color.CYAN);
-        g.fillRect(x,y,50,50);
+        g.fillRect(x, y, 50, 50);
     }
 
     @Override
@@ -34,57 +32,37 @@ public class Gra extends JPanel implements KeyListener, ActionListener{
 
     @Override
     public void keyTyped(KeyEvent e) {
-
-
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        //sterowanie
-        if(e.getKeyCode() == KeyEvent.VK_A){
-            this.x-=poruszanie;
-            System.out.println(x);
-            if(x <= 0){
-                x = 0;
-            }else{
-                x-=poruszanie;
-            }
-            //odświeżanie ekranu, rysowanie aktualnej planczy
-            repaint();
-        }if(e.getKeyCode() == KeyEvent.VK_D){
-
-            System.out.println(x);
-            if(x ==600){
-                x = 600;
-            }else{
-                x+=poruszanie;
-            }
-            //odświeżanie ekranu, rysowanie aktualnej planczy
-            repaint();
-        }if(e.getKeyCode() == KeyEvent.VK_S){
-
-            System.out.println(x);
-            //odświeżanie ekranu, rysowanie aktualnej planczy
-            if(y <=0){
-                y = 0;
-            }else{
-                y+=poruszanie;
+        if (e.getKeyCode() == KeyEvent.VK_A) {
+            if (x > 0) {
+                x -= poruszanie;
             }
             repaint();
-        }if(e.getKeyCode() == KeyEvent.VK_W){
-
-            if(y > 600){
-                y = 600;
-            }else{
-                y-=poruszanie;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_D) {
+            if (x < 650) {
+                x += poruszanie;
             }
-            //odświeżanie ekranu, rysowanie aktualnej planczy
+            repaint();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_S) {
+            if (y < 550) {
+                y += poruszanie;
+            }
+            repaint();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_W) {
+            if (y > 0) {
+                y -= poruszanie;
+            }
             repaint();
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
     }
 }
