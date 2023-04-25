87a1a3e0eb33a90b305ca238b7b0b225e793c89d
Michał Pilecki <michal190713@gmail.com>
2023-04-25 21:36:46 +0200

    dodanie mechaniki poruszania się wrogów i sprwadzania czy wróg jest blisko gracza

--- src/Gra.java
+++ src/Gra.java
@@ -4,11 +4,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 public class Gra extends JPanel implements KeyListener, ActionListener {
     private int x = 300, y = 470, poruszanie = 20;
     private Timer time;
-
+    private int ex = 10, ey = 10;
+    private Random liczba = new Random();
+    private int wrogX1 = liczba.nextInt(6)*50;
+    private int wrogX2 = liczba.nextInt(6)*50;
+    private int wrogY1 = liczba.nextInt(6)*50;
+    private int wrogY2 = liczba.nextInt(6)*50;
     public Gra() {
         time = new Timer(10, this);
         time.start();
@@ -21,13 +27,25 @@ public class Gra extends JPanel implements KeyListener, ActionListener {
     public void paint(Graphics g) {
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, 700, 600);
+        //gracz
         g.setColor(Color.CYAN);
         g.fillRect(x, y, 50, 50);
+        //wróg
+        g.setColor(Color.RED);
+        g.fillRect(wrogX1,ey,60,50);
+        g.fillRect(wrogX2,ey,60,50);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         repaint();
+        ey+=2;
+        Rectangle gracz = new Rectangle(x,475,50,50);
+        Rectangle wrog1 = new Rectangle(wrogX1, ey, 60,50);
+        Rectangle wrog2 = new Rectangle(wrogX2, ey, 60,50);
+        if(gracz.intersects(wrog1)||gracz.intersects(wrog2)){
+            System.out.println("dotyka");
+        }
     }
 
     @Override
