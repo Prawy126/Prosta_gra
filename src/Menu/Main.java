16dcd598b6216b4d89ad0810910c8ae7184ae376
Prawy_126 <michal190713@gmail.com>
2023-05-18 22:41:35 +0200

    dodanie obsługi myszki

--- src/Menu/Main.java
+++ src/Menu/Main.java
@@ -3,13 +3,20 @@ package Menu;
 import javax.swing.*;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         JFrame menu = new JFrame("Menu");
-        menu.setBounds(300,300,800,740);
+        menu.setBounds(300, 300, 800, 740);
         menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         menu.setResizable(false);
-        menu.setVisible(true);
+
         Manu obraz = new Manu();
         menu.add(obraz);
+        JPanel jPanel = new JPanel();
+        jPanel.addMouseListener(obraz);
+
+
+        menu.add(jPanel);
+
+        menu.setVisible(true);
     }
 }
